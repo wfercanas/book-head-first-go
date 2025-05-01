3ee2baeff6814e472436d036ff269edb63e1a7a1
Fernando Cañas <fernando.canas@outlook.com>
2025-05-01 09:34:02 -0500

    refactor: use function to calculate paint needed

--- 3-functions/painting/paint.go
+++ 3-functions/painting/paint.go
@@ -2,15 +2,12 @@ package main
 
 import "fmt"
 
-func main() {
-	var width, height, area float64
-	width = 4.2
-	height = 3.0
-	area = width * height
-	fmt.Println(area, "liters needed")
+func paintNeeded(width, height float64) {
+	area := width * height
+	fmt.Printf("%.2f liters needed\n", area/10)	// 1.26
+}
 
-	width = 5.2
-	height = 3.5
-	area = width * height
-	fmt.Println(area, "liters needed")
+func main() {
+	paintNeeded(4.2, 3.0)	
+	paintNeeded(5.2, 3.5)
 }
