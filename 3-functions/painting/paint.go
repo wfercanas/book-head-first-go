c442ced2c68d7d92c5d15fd8cfd3cc4e4848bdb7
Fernando Cañas <fernando.canas@outlook.com>
2025-05-01 11:17:15 -0500

    refactor: add error handling when calculating paint

--- 3-functions/painting/paint.go
+++ 3-functions/painting/paint.go
@@ -1,18 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-func paintNeeded(width, height float64) float64 {
+func paintNeeded(width, height float64) (float64, error) {
+	if height < 0 {
+		return 0, fmt.Errorf("%.2f is an invalid height", height)
+	}
+	if width < 0 {
+		return 0, fmt.Errorf("%.2f is an invalid width", width)
+	}
+
 	area := width * height
-	return area / 10
+	return area / 10, nil
 }
 
 func main() {
 	var total, amount float64
-	amount = paintNeeded(4.2, 3.0)	
+	var err error
+
+	amount, err = paintNeeded(4.2, 3.0)	
+	if err != nil {
+		log.Fatal(err)
+	}
 	total += amount
 	fmt.Printf("%.2f liters needed\n", amount)
-	amount = paintNeeded(5.2, 3.5)
+
+	amount, err = paintNeeded(5.2, 3.5)
+	if err != nil {
+		log.Fatal(err)
+	}
 	total += amount
 	fmt.Printf("%.2f liters needed\n", amount)
 	fmt.Printf("Total: %.2f liters\n", total)
