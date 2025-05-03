a7341b680225d86a72d32ea7764bf4cf45a5ab48
Fernando Cañas <fernando.canas@outlook.com>
2025-05-03 12:16:07 -0500

    feat: greeting package exercise

---
+++ 4-packages/greeting/greet/greet.go
@@ -0,0 +1,11 @@
+package greet
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello!")
+}
+
+func Hi() {
+	fmt.Println("Hi!")
+}
---
+++ 4-packages/greeting/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"greeting/greet"
+)
+
+func main() {
+	greet.Hello()
+	greet.Hi()
+}
