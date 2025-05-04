87f92a844b4bcf5e16bb34be63748dc569d21114
Fernando Cañas <fernando.canas@outlook.com>
2025-05-04 12:07:09 -0500

    feat: average exercise reading from files

--- 5-arrays/average/average.go
+++ 5-arrays/average/average.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"average/datafile"
+	"fmt"
+	"log"
+)
 
 func main() {
-	numbers := [3]float64{71.8, 56.2, 89.5}
+	numbers, err := datafile.GetFloat("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	var sum float64 = 0
 
 	for _, value := range numbers {
---
+++ 5-arrays/average/go.mod
@@ -0,0 +1,3 @@
+module average
+
+go 1.23.3
