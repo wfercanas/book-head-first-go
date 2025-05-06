66738fa06a786c95499aa77831035d5605585f59
Fernando Cañas <fernando.canas@outlook.com>
2025-05-06 17:08:10 -0500

    feat: code magnets exercise about variadic sum

---
+++ 6-slices/sum/sum.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func sum(numbers ...int) int {
+	var sum int = 0
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println(sum(9, 7))
+	fmt.Println(sum(4, 2, 1))
+}
