673060177b95a803323241853d0885528e78b178
Fernando Cañas <fernando.canas@outlook.com>
2025-05-09 22:27:02 -0500

    feat: create printInfo and defaultSubscriber functions to work with subscriber type

--- 8-structs/subscriptions/subscribe.go
+++ 8-structs/subscriptions/subscribe.go
@@ -8,13 +8,23 @@ type subscriber struct {
 	active bool
 }
 
-func main() {
-	var subscriber1 subscriber
-	subscriber1.name = "Aman Singh"
-	subscriber1.rate = 4.99
-	subscriber1.active = true
+func printInfo(s subscriber) {
+	fmt.Println("Name:", s.name)
+	fmt.Println("Rate:", s.rate)
+	fmt.Println("Active:", s.active)
+}
 
-	fmt.Println("Name:", subscriber1.name)
-	fmt.Println("Rate:", subscriber1.rate)
-	fmt.Println("Active:", subscriber1.active)
+func defaultSubscriber(name string) subscriber {
+	var s subscriber
+	s.name = name
+	s.rate = 5.99
+	s.active = true
+	return s
+}
+
+func main() {
+	subscriber1 := defaultSubscriber("Aman Singh")
+	subscriber2 := defaultSubscriber("Beth Ryan")
+	printInfo(subscriber1)
+	printInfo(subscriber2)
 }
