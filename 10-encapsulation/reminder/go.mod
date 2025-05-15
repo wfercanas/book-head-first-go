275781a8be198bfa3309d8e955076c1d0f006a5c
Fernando Cañas <fernando.canas@outlook.com>
2025-05-14 22:20:19 -0500

    feat: move Date type to calendar package and add getter methods

---
+++ 10-encapsulation/reminder/go.mod
@@ -0,0 +1,3 @@
+module reminder
+
+go 1.23.3
--- 10-encapsulation/reminder/reminder.go
+++ 10-encapsulation/reminder/reminder.go
@@ -1,57 +1,29 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
+	"reminder/calendar"
 )
 
-type Date struct {
-	Year  int
-	Month int
-	Day   int
-}
-
-func (d *Date) setYear(year int) error {
-	if year < 1 {
-		return errors.New("invalid year")
-	}
-	d.Year = year
-	return nil
-}
-
-func (d *Date) setMonth(month int) error {
-	if month < 1 || month > 12 {
-		return errors.New("invalid month")
-	}
-	d.Month = month
-	return nil
-}
-
-func (d *Date) setDay(day int) error {
-	if day < 1 || day > 31 {
-		return errors.New("invalid day")
-	}
-	d.Day = day
-	return nil
-}
-
 func main() {
-	date := Date{}
-	err := date.setYear(2019)
+	date := calendar.Date{}
+	err := date.SetYear(2019)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = date.setMonth(5)
+	err = date.SetMonth(5)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = date.setDay(15)
+	err = date.SetDay(15)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(date)
+	fmt.Println(date.Day())
+	fmt.Println(date.Month())
+	fmt.Println(date.Year())
 }
