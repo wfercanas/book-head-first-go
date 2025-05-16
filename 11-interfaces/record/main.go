468abc8302a8405258172b52bfea797fa6e9688f
Fernando Cañas <fernando.canas@outlook.com>
2025-05-16 16:04:29 -0500

    feat: type assertions handling errors

--- 11-interfaces/record/main.go
+++ 11-interfaces/record/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "tape/gadget"
+import (
+	"fmt"
+	"tape/gadget"
+)
 
 type Player interface {
 	Play(string)
@@ -14,11 +17,24 @@ func playList(device Player, songs []string) {
 	device.Stop()
 }
 
+func tryOut(device Player) {
+	device.Play("One more time")
+	device.Stop()
+	recorder, ok := device.(gadget.TapeRecorder)
+	if ok {
+		recorder.Record()
+	} else {
+		fmt.Println("Player is not a recorder")
+	}
+}
+
 func main() {
 	var player Player
 	player = gadget.TapeRecorder{}
 	mixtape := []string{"Jessie's Girl", "Whip it", "9 to 5"}
 	playList(player, mixtape)
+	tryOut(player)
 	player = gadget.TapePlayer{}
 	playList(player, mixtape)
+	tryOut(player)
 }
