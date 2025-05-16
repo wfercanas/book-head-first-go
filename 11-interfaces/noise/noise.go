c99122f3f2ecb8799460fabc9f4d59a654728794
Fernando Cañas <fernando.canas@outlook.com>
2025-05-16 14:19:39 -0500

    feat: variables and function parameters defined with interface types

---
+++ 11-interfaces/noise/noise.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Whistle string
+
+func (w Whistle) MakeSound() {
+	fmt.Println("Tweet!")
+}
+
+type Horn string
+
+func (h Horn) MakeSound() {
+	fmt.Println("Honk!")
+}
+
+type NoiseMaker interface {
+	MakeSound()
+}
+
+func Play(n NoiseMaker) {
+	n.MakeSound()
+}
+
+func main() {
+	var toy NoiseMaker
+	toy = Whistle("Canary")
+	toy.MakeSound()
+	Play(toy)
+
+	toy = Horn("Blaster")
+	toy.MakeSound()
+	Play(toy)
+}
