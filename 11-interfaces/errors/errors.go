92528188c1e13d96bddb642e82b290496f0cbcf4
Fernando Cañas <fernando.canas@outlook.com>
2025-05-16 21:40:10 -0500

    feat: errors interface

---
+++ 11-interfaces/errors/errors.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type OverheatError float64
+
+func (o OverheatError) Error() string {
+	return fmt.Sprintf("Overheating by %.1f degrees", o)
+}
+
+func checkTemperature(actual float64, safe float64) error {
+	excess := actual - safe
+	if excess > 0 {
+		return OverheatError(excess)
+	}
+	return nil
+}
+
+func main() {
+	var err error = checkTemperature(121.3, 100)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
