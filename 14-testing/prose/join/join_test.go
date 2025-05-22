0658d93e1ca8ade0dce616c02e5b9fb9f6ebe627
Fernando Cañas <fernando.canas@outlook.com>
2025-05-22 14:47:46 -0500

    feat: tests for JoinWithCommas using two and three elements

--- 14-testing/prose/join/join_test.go
+++ 14-testing/prose/join/join_test.go
@@ -3,9 +3,15 @@ package join
 import "testing"
 
 func TestTwoElements(t *testing.T) {
-	// t.Error("Test not implemented")
+	list := []string{"apple", "orange"}
+	if JoinWithCommas(list) != "apple and orange" {
+		t.Error("didn't match expected value")
+	}
 }
 
 func TestThreeElements(t *testing.T) {
-	// t.Error("Test not implemented")
+	list := []string{"apple", "orange", "pear"}
+	if JoinWithCommas(list) != "apple, orange, and pear" {
+		t.Error("didn't match expected value")
+	}
 }
