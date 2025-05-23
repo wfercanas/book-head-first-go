ef71e2cf60f3d5cc648d845a74994ef813426d73
Fernando Cañas <fernando.canas@outlook.com>
2025-05-22 22:54:21 -0500

    feat: create function that receives a callback

--- 15-webapps/first-class-func/func.go
+++ 15-webapps/first-class-func/func.go
@@ -6,8 +6,18 @@ func sayHi() {
 	fmt.Println("Hi")
 }
 
+func sayBye() {
+	fmt.Println("Bye")
+}
+
+func twice(callback func()) {
+	callback()
+	callback()
+}
+
 func main() {
-	var hello func()
-	hello = sayHi
-	hello()
+	var hello func() = sayHi
+	goodbye := sayBye
+	twice(hello)
+	twice(goodbye)
 }
