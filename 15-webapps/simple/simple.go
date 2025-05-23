83cd788ce497ca5bcc39e36864dc07a4e5356a94
Fernando Cañas <fernando.canas@outlook.com>
2025-05-22 22:47:34 -0500

    feat: simple web server with three resource paths

--- 15-webapps/simple/simple.go
+++ 15-webapps/simple/simple.go
@@ -5,16 +5,29 @@ import (
 	"net/http"
 )
 
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	message := []byte("Hello World")
+func write(writer http.ResponseWriter, message []byte) {
 	_, err := writer.Write(message)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func englishHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, []byte("Hello"))
+}
+
+func hindiHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, []byte("Namaste"))
+}
+
+func frenchHandler(writer http.ResponseWriter, request *http.Request) {
+	write(writer, []byte("Salut"))
+}
+
 func main() {
-	http.HandleFunc("/hello", viewHandler)
+	http.HandleFunc("/hello", englishHandler)
+	http.HandleFunc("/hindi", hindiHandler)
+	http.HandleFunc("/salut", frenchHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
